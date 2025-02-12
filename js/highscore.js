fdcda3ff4d43bdfe4d972ba2ec0f1c9cdc4bdf6b
Peter <151863071+peter8237@users.noreply.github.com>
2025-02-12 01:55:41 +0100

    Update highscore.js

--- js/highscore.js
+++ js/highscore.js
@@ -1,1 +1,18 @@
+function updateScores() {
+    scoreEl.textContent = "Score: " + score;
+    highscoreEl.textContent = "Highscore: " + highscore;
+}
 
+function saveHighscore() {
+    if (score > highscore) {
+        highscore = score;
+        localStorage.setItem("highscore", highscore);
+        updateScores();
+    }
+}
+/* die funktion resetHighscore kann später auch entfernt werden, dient nur zu debugging zwecken, spart das tippen in der console */
+function resetHighscore() {
+    highscore = 0;
+    localStorage.removeItem("highscore");
+    updateScores();
+}
