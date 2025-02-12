f4f5fedbc1a0aaddad19bf779bafc68b6e44638f
Peter <151863071+peter8237@users.noreply.github.com>
2025-02-12 01:53:52 +0100

    Delete js/background.js

--- js/background.js
+++
@@ -1,9 +0,0 @@
-function moveBackground() {
-    let position = 0;
-    function animate() {
-        position += 2;
-        gameBackgroundElement.style.backgroundPosition = `${position}px 0`;
-        requestAnimationFrame(animate);
-    }
-    animate();
-}
