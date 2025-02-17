aff147409b6d8b277194700c7b20b7746b6429c3
Peter <151863071+peter8237@users.noreply.github.com>
2025-02-17 20:38:43 +0100

    Update config.js
    Änderungen zur Namensspeicherung für Highscore hinzugefügt

--- js/config.js
+++ js/config.js
@@ -1,22 +1,24 @@
-let countdownText = ["3", "2", "1", "LOS"],
-    countdownColors = ["white", "yellow", "orange", "red"], countdownInterval = 500, countdownInProgress = false;
+let countdownInProgress = false, newHighscoreInput = false,
+    countdownText = ["3", "2", "1", "LOS"], countdownColors = ["white", "yellow", "orange", "red"],
     gameOver = false, onGround = true, gameStarted = false, countdownDone = false,
     speedMin = 15, speedMax = 35, score = 0, enemyPos = 0, enemySpeed = 0, bgSpeed = 3,
-    highscore = localStorage.getItem("highscore") ? +localStorage.getItem("highscore") : 0; 
+    highscore = localStorage.getItem("highscore") ? +localStorage.getItem("highscore") : 0;
 
-const enemyEl = getEl("enemyCharacter"),
+const countdownInterval = 200,
+    enemyEl = getEl("enemyCharacter"),
     playerEl = getEl("playerCharacter"),
     startMenuEl = getEl("startMenu"),
     bgEl = getEl("gameBackground"),
     scoreEl = getEl("currentScoreDisplay"),
     highscoreEl = getEl("highscoreDisplay"),
     countdownEl = getEl("startMenuContent"),
+    inputFieldEl = document.getElementById("inputField"),
     playerXPos = +getComputedStyle(document.documentElement).getPropertyValue("--player-xpos"), //+ entspricht parseInt()
     sfx_jump = new Audio("sfx/jump.mp3"),
     sfx_dead = new Audio("sfx/death.mp3"),
     sfx_music = new Audio("sfx/music_p.mp3");
 
-    sfx_music.loop = true;
+sfx_music.loop = true;
 
 function getEl(id) {
     return document.getElementById(id);
