79336bf91746f84447bfdc9b0869f98e98bed855
Peter <151863071+peter8237@users.noreply.github.com>
2025-02-22 16:29:58 +0100

    Update countdown.js
    änderungen zur implementierung des highscore ranking systems

--- js/countdown.js
+++ js/countdown.js
@@ -1,19 +1,22 @@
-
 function startCountdown() {
+    highscoresEl.style.display = "none";
+    countdownEl.style.display = "none";
     countdownInProgress = true;
-    for (let i = 0; i < countdownText.length; i++) {
-        setTimeout(function () {
-            countdownEl.textContent =  countdownText[i];
-            countdownEl.style.color = countdownColors[i];
-            countdownEl.style.fontSize = (i*2  + 1.5) + "vw";
-        }, i * countdownInterval);
-    }
-    setTimeout(function () {
+    setTimeout(() => {
+        countdownEl.style.display = "flex"; // flackern fix
+    }, countdownInterval);
+    countdownText.forEach((text, index) => {
+        setTimeout(() => {
+            countdownEl.textContent = text;
+            countdownEl.style.color = countdownColors[index];
+            countdownEl.style.fontSize = (index * 2 + 1.5) + "vw";
+        }, index * countdownInterval);
+    });
+    setTimeout(() => {
         countdownDone = true;
         countdownInProgress = false;
         countdownEl.style.color = "white";
         countdownEl.textContent = "";
         startGame();
     }, countdownText.length * countdownInterval);
-
 }
