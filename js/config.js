5fcecd7394fb04b1f3a935b2ebd7e23c8d593492
Peter <151863071+peter8237@users.noreply.github.com>
2025-02-22 16:29:03 +0100

    Update config.js
    änderungen zur implementierung des highscore ranking systems

--- js/config.js
+++ js/config.js
@@ -1,19 +1,35 @@
-let countdownInProgress = false, newHighscoreInput = false,
-    countdownText = ["3", "2", "1", "LOS"], countdownColors = ["white", "yellow", "orange", "red"],
-    gameOver = false, onGround = true, gameStarted = false, countdownDone = false,
-    speedMin = 15, speedMax = 35, score = 0, enemyPos = 0, enemySpeed = 0, bgSpeed = 3,
-    highscore = localStorage.getItem("highscore") ? +localStorage.getItem("highscore") : 0;
+const noEntryYet ="(╯°□°）╯︵ ┻━┻",
+    countdownText = ["3", "2", "1", "LOS"],
+    countdownColors = ["white", "yellow", "orange", "red"],
+    maxTopEntries = 4,
+    countdownInterval = 420,
+    altertDistance = 777,
+    speedMin = 15,
+    speedMax = 35,
+    bgSpeed = 3;
 
-const countdownInterval = 420,
-    enemyEl = getEl("enemyCharacter"),
+let countdownInProgress = false,
+    newHighscoreInput = false,
+    gameOver = false,
+    onGround = true,
+    gameStarted = false,
+    countdownDone = false,
+    score = 0,
+    enemyPos = 0,
+    enemySpeed = 0;
+
+const enemyEl = getEl("enemyCharacter"),
     playerEl = getEl("playerCharacter"),
     startMenuEl = getEl("startMenu"),
     bgEl = getEl("gameBackground"),
     scoreEl = getEl("currentScoreDisplay"),
-    highscoreEl = getEl("highscoreDisplay"),
     countdownEl = getEl("startMenuContent"),
-    inputFieldEl = document.getElementById("inputField"),
-    playerXPos = +getComputedStyle(document.documentElement).getPropertyValue("--player-xpos"), //+ entspricht parseInt()
+    inputFieldEl = getEl("inputField"),
+    playerXPos = +getComputedStyle(document.documentElement).getPropertyValue("--player-xpos"), // + ist die kurzversion von parseInt()
+    jumpDuration = +getComputedStyle(document.documentElement).getPropertyValue("--jump-duration"),
+    playerWidth = +getComputedStyle(document.documentElement).getPropertyValue("--player-width"),
+    enemyWidth = +getComputedStyle(document.documentElement).getPropertyValue("--enemy-width"),
+    highscoresEl = getEl("topHighscores"),
     sfx_jump = new Audio("sfx/jump.mp3"),
     sfx_dead = new Audio("sfx/death.mp3"),
     sfx_music = new Audio("sfx/music_p.mp3");
@@ -22,8 +38,4 @@ sfx_music.loop = true;
 
 function getEl(id) {
     return document.getElementById(id);
-}
-
-function randSpeed(min, max) {
-    return Math.random() * (max - min) + min;
 }
