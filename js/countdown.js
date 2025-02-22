0cccfa32edd3402d60ffa8b13e38805891c42476
Peter <151863071+peter8237@users.noreply.github.com>
2025-02-22 17:18:37 +0100

    Update countdown.js
    countdown starts at 3 now instead of 2

--- js/countdown.js
+++ js/countdown.js
@@ -5,18 +5,20 @@ function startCountdown() {
     setTimeout(() => {
         countdownEl.style.display = "flex"; // flackern fix
     }, countdownInterval);
+
     countdownText.forEach((text, index) => {
         setTimeout(() => {
             countdownEl.textContent = text;
             countdownEl.style.color = countdownColors[index];
             countdownEl.style.fontSize = (index * 2 + 1.5) + "vw";
-        }, index * countdownInterval);
+        }, (index + 1) * countdownInterval);
     });
+
     setTimeout(() => {
         countdownDone = true;
         countdownInProgress = false;
         countdownEl.style.color = "white";
         countdownEl.textContent = "";
         startGame();
-    }, countdownText.length * countdownInterval);
+    }, (countdownText.length + 1) * countdownInterval);
 }
