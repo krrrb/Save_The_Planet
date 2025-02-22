9c1e9b432d19e3b89f84c5400b79071d0963edec
Peter <151863071+peter8237@users.noreply.github.com>
2025-02-22 16:30:24 +0100

    Update game.js
    änderungen zur implementierung des highscore ranking systems

--- js/game.js
+++ js/game.js
@@ -1,8 +1,12 @@
-document.addEventListener("keydown", function (event) {
-    let code = event.code;
-    if (code === "F4") {
-        resetHighscore();
-    } else if ((code === "Space" || code === "KeyW" || code === "ArrowUp") && (countdownInProgress === false)&& (newHighscoreInput === false)) {
+function randSpeed(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function handleKeyDown(event) {
+    const code = event.code;
+    if (code === "F2") {
+        deleteLastHighscore();
+    } else if ((code === "Space" || code === "KeyW" || code === "ArrowUp") && !countdownInProgress && !newHighscoreInput) {
         if (gameStarted) {
             jump();
         } else if (!countdownDone) {
@@ -10,11 +14,11 @@ document.addEventListener("keydown", function (event) {
         } else {
             startGame();
         }
-    } else if ((code === "Enter" || code === "NumpadEnter") && (newHighscoreInput === true)) {
-            saveHighscorePlayerName();
-            newHighscoreInput = false;
+    } else if ((code === "Enter" || code === "NumpadEnter") && newHighscoreInput) {
+        saveHighscorePlayerName();
+        newHighscoreInput = false;
     }
-});
+}
 
 function startGame() {
     sfx_music.play();
@@ -38,23 +42,23 @@ function jump() {
     playerEl.classList.add("jump");
     playerEl.style.backgroundImage = "var(--player-jump)";
     onGround = false;
-    setTimeout(function () {
+    setTimeout(() => {
         playerEl.classList.remove("jump");
         playerEl.style.backgroundImage = "var(--player-ground)";
         onGround = true;
-    }, 600);
+    }, jumpDuration);
 }
 
 function moveEnemy() {
     if (gameOver) return;
-    if (enemyPos < 900 + playerXPos && onGround) {
+    if (enemyPos < altertDistance + playerXPos && onGround) {
         playerEl.style.backgroundImage = "var(--player-alert)";
         enemyEl.style.backgroundImage = "var(--enemy-alert)";
     }
     enemyEl.style.left = (enemyPos -= enemySpeed) + "px";
-    if (enemyPos < 120 + playerXPos && onGround) {
+    if (enemyPos < playerWidth + playerXPos && onGround) {
         handleGameOver();
-    } else if (enemyPos < 0) {
+    } else if (enemyPos < -enemyWidth/4) { // durch 4 sodass nur dar asteroid (der schweif ist ca 3/4) kollisionen auslöst. dirty fix, kollision wird eigentlich weiter geprüft aber nach 1/4 wird der asteroid zurückgesetzt.
         enemyPos = window.innerWidth;
         score++;
         updateScores();
@@ -65,14 +69,15 @@ function moveEnemy() {
 }
 
 function handleGameOver() {
-    let newHighscoreMessage =
-    `<span id="inputPrompt1">Du hast zwar über 8 Millarden Menschen auf dem Gewissen,<br><br>
-    aber zumindest hast du einen neuen highscore von ${score} aufgestellt!<br><br>
-    Verewige dich in den Trümmern zu und bestätige mit <span style="color: red;">Enter</span>.</span><br><br>
-    <span id="inputPrompt2" style="display: none">Um es nochmals zu versuchen, drücke die <span style="color: orange;">Leertaste</span>.</span>`;
-    const noNewHighscoreMessage = 
-    `<span>Du hast über 8 Millarden Menschen auf dem Gewissen!<br><br>
-    Um es nochmals zu versuchen, drücke die <span style="color: orange;">Leertaste</span></span>.`;
+    const newHighscoreMessage = `
+        <span id="inputPrompt1">Du hast zwar über 8 Milliarden Menschen auf dem Gewissen,<br><br>
+        aber zumindest hast du einen neuen Highscore von ${score} aufgestellt!<br><br>
+        Verewige dich in den Trümmern und bestätige mit <span style="color: red;">Enter</span>.<br><br></span>
+        <span id="inputPrompt2" style="display: none"><br>Um es nochmals zu versuchen, drücke die <span style="color: orange;">Leertaste</span>.</span>`;
+    const noNewHighscoreMessage = `
+        <span><br>Du hast über 8 Milliarden Menschen auf dem Gewissen!<br><br>
+        Um es nochmals zu versuchen, drücke die <span style="color: orange;">Leertaste</span>.</span>`;
+
     sfx_music.pause();
     sfx_music.currentTime = 0;
     sfx_dead.play();
@@ -80,20 +85,19 @@ function handleGameOver() {
     enemyEl.style.backgroundImage = "var(--enemy-collision)";
     gameOver = true;
 
-    if (score > highscore) {
+    if (isNewHighscore(score)) {
         countdownEl.innerHTML = newHighscoreMessage;
         inputFieldEl.style.display = "block";
         inputFieldEl.value = "";
-        saveHighscore();
-        setTimeout(function () {
+        setTimeout(() => {
             startMenuEl.style.display = "flex";
-            updateScores();
             gameStarted = false;
             countdownDone = false;
             inputFieldEl.focus();
         }, 1000);
-    newHighscoreInput = true; 
+        newHighscoreInput = true;
     } else {
+        highscoresEl.style.display = "block";
         newHighscoreInput = false;
         startMenuEl.style.display = "flex";
         countdownEl.innerHTML = noNewHighscoreMessage;
